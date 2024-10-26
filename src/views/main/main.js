7ffb91d2613521b1005d0f791294c796e1ea711c
alivka-oko <alina.okotnikova2002@gmail.com>
2024-10-26 16:48:26 +0300

    state

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,15 +1,31 @@
 import { AbtractView } from "../../common/view";
+import onChange from "on-change";
 
 export class MainView extends AbtractView {
-  constructor() {
+  state = {
+    list: [],
+    loading: false,
+    searchQuery: undefined,
+    offset: 0,
+  };
+  constructor(appState) {
     super();
+    this.appState = appState;
+    this.appState = onChange(this.appState, this.AppStateHook.bind(this));
     this.setTitle("Поиск книг");
+  }
+
+  AppStateHook(path) {
+    if (path === "favorites") {
+      console.log(path);
+    }
   }
 
   render() {
     const main = document.createElement("div");
-    main.innerHTML = "Тест";
+    main.innerHTML = `Число книг: ${this.appState.favorites.length}`;
     this.app.innerHTML = "";
     this.app.append(main);
+    this.appState.favorites.push("123");
   }
 }
