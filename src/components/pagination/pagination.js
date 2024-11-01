dd69cee3b2b3be115ebdfefd4e23b15c1a17b0fa
alivka-oko <alina.okotnikova2002@gmail.com>
2024-11-01 14:06:26 +0300

    update pagination logic

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -10,22 +10,38 @@ export class Pagination extends DivComponent {
   render() {
     this.el.classList.add("pagination");
     this.el.innerHTML = `
-          <button class="pagination__button" id="pagination-before">
+          <button class="pagination__button" id="pagination-previous">
              Предыдущая страница
           </button>
           <button class="pagination__button" id="pagination-next">
              Следующая страница
           </button>
     `;
+    const previousButton = this.el.querySelector("#pagination-previous");
+    const nextButton = this.el.querySelector("#pagination-next");
+
+    previousButton.disabled = true;
+    if (
+      this.state.list.length < this.state.limit ||
+      this.state.list.length === this.state.numFound
+    ) {
+      nextButton.disabled = true;
+    }
+    if (this.state.page > 1) {
+      previousButton.disabled = false;
+    }
+
     this.el.addEventListener("click", (e) => {
       if (e.target.tagName !== "BUTTON") {
         return;
       }
       const purposeOfButton = e.target.id;
-      if (purposeOfButton === "pagination-before") {
+      if (purposeOfButton === "pagination-previous") {
         this.state.page > 1 ? this.state.page-- : this.state.page;
       } else if (purposeOfButton === "pagination-next") {
-        this.state.page++;
+        if (this.state.numFound > this.state.limit) {
+          this.state.page++;
+        }
       }
     });
     return this.el;
